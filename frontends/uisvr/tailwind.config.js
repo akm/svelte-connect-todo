a8890db44906f1bf2561414450dd4e786df3f2ca
akm <akm2000@gmail.com>
2024-04-16 08:17:30 +0900

    :wrench: Add content and plugins to tailwind.config.js

--- frontends/uisvr/tailwind.config.js
+++ frontends/uisvr/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{html,svelte,js,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 }
-
