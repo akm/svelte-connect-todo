2468155d6b5adc33e951a13f66e2cb6457aa2377
akm <akm2000@gmail.com>
2024-04-15 09:31:26 +0900

    :sparkles: Apply class for skelton and remove styles defined in .svelte

--- frontends/uisvr/src/routes/+page.svelte
+++ frontends/uisvr/src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import type { Task } from '$lib/models/task';
-	import type { ServerLoadEvent } from '@sveltejs/kit';
 	import { TaskService } from '../gen/task/v1/task_connect';
 	import { createPromiseClient } from '@connectrpc/connect';
 	import { createConnectTransport } from '@connectrpc/connect-web';
@@ -12,12 +11,12 @@
 	const client = createPromiseClient(TaskService, transport);
 </script>
 
-<div class="centered">
-	<h1>todos</h1>
+<div class="space-y-4 mt-8 mx-8">
+	<h1 class="h1">todos</h1>
 
-	<label>
+	<label class="label">
 		add a todo:
-		<input
+		<input class="input"
 			type="text"
 			autocomplete="off"
 			on:keydown={async (e) => {
@@ -38,8 +37,8 @@
 	<ul class="todos">
 		{#each data.tasks as task (task.id)}
 			<li>
-				<label>
-					<input
+				<label class="label flex items-center space-x-2">
+					<input class="checkbox"
 						type="checkbox"
 						checked={task.done}
 						on:change={async (e) => {
@@ -47,52 +46,16 @@
 							await client.update({ id: task.id, name: task.name, status: done ? Status.DONE : Status.TODO });
 						}}
 					/>
-					<span>{task.name}</span>
-					<button
+					<p>{task.name}</p>
+					<button type="button" class="btn btn-sm variant-filled"
 						aria-label="Mark as complete"
 						on:click={async (e) => {
 							await client.delete({ id: task.id });
 							data.tasks = data.tasks.filter((t) => t !== task);
 						}}
-					/>
+					>delete</button>
 				</label>
 			</li>
 		{/each}
 	</ul>
 </div>
-
-<style>
-	.centered {
-		max-width: 20em;
-		margin: 0 auto;
-	}
-
-	label {
-		display: flex;
-		width: 100%;
-	}
-
-	input[type='text'] {
-		flex: 1;
-		color: black;
-	}
-
-	span {
-		flex: 1;
-	}
-
-	button {
-		border: none;
-		background: url(./remove.svg) no-repeat 50% 50%;
-		background-size: 1rem 1rem;
-		cursor: pointer;
-		height: 100%;
-		aspect-ratio: 1;
-		opacity: 0.5;
-		transition: opacity 0.2s;
-	}
-
-	button:hover {
-		opacity: 1;
-	}
-</style>
