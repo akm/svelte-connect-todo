b9e50232f9ae385638472aefc64cb4eba5a62601
akm <akm2000@gmail.com>
2024-04-16 07:21:20 +0900

    :sparkles: Add @tailwindcss/forms plugin

--- frontends/uisvr/tailwind.config.ts
+++ frontends/uisvr/tailwind.config.ts
@@ -4,6 +4,9 @@ import type { Config } from 'tailwindcss';
 // 1. Import the Skeleton plugin
 import { skeleton } from '@skeletonlabs/tw-plugin';
 
+// See https://www.skeleton.dev/elements/forms
+import forms from '@tailwindcss/forms';
+
 const config = {
 	// 2. Opt for dark mode to be handled via the class method
 	darkMode: 'class',
@@ -19,7 +22,8 @@ const config = {
 		// 4. Append the Skeleton plugin (after other plugins)
 		skeleton({
 			themes: { preset: ['skeleton'] }
-		})
+		}),
+		forms
 	]
 } satisfies Config;
 
