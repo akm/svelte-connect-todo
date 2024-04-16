da2d7c071e738113e88e9856c4bbd60864a8ff48
akm <akm2000@gmail.com>
2024-04-16 09:43:35 +0900

    :sparkles: Add signup and signin pages

---
+++ frontends/uisvr/src/routes/(public)/signin/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+
+	let email = '';
+	let password = '';
+	let errorMessage = '';
+
+	const signin = async () => {
+	};
+
+	const signinOnEnter = (e: KeyboardEvent) => {
+	};
+	const clearErrorMessage = () => {
+		errorMessage = '';
+	};
+</script>
+
+<div class="prose space-y-4 m-8">
+	<h1 class="mb-4">Sign in</h1>
+
+	{#if errorMessage}
+		<div class="flex">
+			<Icon icon="exclamation-mark-fill" />
+			<p class="text-red-500 ml-2">
+				{errorMessage}
+			</p>
+		</div>
+	{/if}
+
+	<label class="block">
+		<span class="text-gray-700">Email</span>
+		<input
+			class="mt-1 block w-full"
+			bind:value={email}
+			placeholder="your email address"
+			on:keypress={signinOnEnter}
+			on:change={clearErrorMessage}
+		/>
+	</label>
+
+	<label class="block">
+		<span class="text-gray-700">Password</span>
+		<input
+			class="mt-1 block w-full"
+			type="password"
+			bind:value={password}
+			placeholder="your password"
+			on:keypress={signinOnEnter}
+			on:change={clearErrorMessage}
+		/>
+	</label>
+	<div class="flex">
+		<button class="flex-none mt-4 h-12" on:click={signin}>Sign in</button>
+		<div>
+			<a class="mt-4 ml-8 h-12" color="alternative" href="/signup">Sign up</a>
+			<div class="ml-8">Sign up if you don't have an account.</div>
+		</div>
+	</div>
+</div>
---
+++ frontends/uisvr/src/routes/(public)/signup/+page.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+
+	let email = '';
+	let password = '';
+	let accountName = '';
+	let errorMessage = '';
+
+	const signup = async () => {
+	};
+
+	const signupOnEnter = (e: KeyboardEvent) => {
+		if (e.key === 'Enter') {
+			signup();
+		}
+	};
+	const clearErrorMessage = () => {
+		errorMessage = '';
+	};
+</script>
+
+<div class="prose space-y-4 m-8">
+	<h1 class="mb-4">Sign up</h1>
+
+	{#if errorMessage}
+		<div class="flex">
+			<Icon icon="exclamation-mark-fill" />
+			<p class="text-red-500 ml-2">
+				{errorMessage}
+			</p>
+		</div>
+	{/if}
+
+	<label class="block">
+		<span class="text-gray-700">Email</span>
+		<input
+			class="mt-1 block w-full"
+			bind:value={email}
+			placeholder="your email address"
+			on:keypress={signupOnEnter}
+			on:change={clearErrorMessage}
+		/>
+	</label>
+
+	<label class="block">
+		<span class="text-gray-700">Passeord</span>
+		<input
+			class="mt-1 block w-full"
+			type="password"
+			bind:value={password}
+			placeholder="your password"
+			on:keypress={signupOnEnter}
+			on:change={clearErrorMessage}
+		/>
+	</label>
+
+	<label class="block">
+		<span class="text-gray-700">Account name</span>
+		<input
+			class="mt-1 block w-full"
+			bind:value={accountName}
+			placeholder="your account name"
+			on:keypress={signupOnEnter}
+			on:change={clearErrorMessage}
+		/>
+		<p class="text-sm">You can change your account name after sign up</p>
+	</label>
+
+	<div class="flex">
+		<button class=" flex-none mt-4 h-12" on:click={signup}>Sign up</button>
+		<div>
+			<a class="mt-4 ml-8 h-12" color="alternative" href="/signin">Sign in</a>
+			<div class="ml-8">Sign in if you already have your account.</div>
+		</div>
+	</div>
+</div>
