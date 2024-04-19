5b7088d4e05f4ebac890e21368b69c09e452c13e
akm <akm2000@gmail.com>
2024-04-19 12:35:27 +0900

    :recycle: Extract a constant cookieKey

--- backends/apisvr/services/auth/session.go
+++ backends/apisvr/services/auth/session.go
@@ -2,7 +2,9 @@ package auth
 
 import "fmt"
 
+const cookieKey = "session"
+
 func SessionCookieValue(idToken string) string {
 	// See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie
-	return fmt.Sprintf("session=%s; Secure; HttpOnly", idToken)
+	return fmt.Sprintf("%s=%s; Secure; HttpOnly", cookieKey, idToken)
 }
