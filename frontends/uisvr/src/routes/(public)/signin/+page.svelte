779fa1b49d4491cfbd97620d3be838f91970e6ca
akm <akm2000@gmail.com>
2024-04-21 13:10:57 +0900

    :sparkles: Implement sign-in page

--- frontends/uisvr/src/routes/(public)/signin/+page.svelte
+++ frontends/uisvr/src/routes/(public)/signin/+page.svelte
@@ -2,14 +2,31 @@
 	import Icon from '@iconify/svelte';
 	import { page } from '$app/stores';
 
+	import {isFirebaseError} from '$lib/firebase';
+	import type {UserCredential} from '$lib/firebase/auth';
+	import { auth, signInWithEmailAndPassword } from "$lib/firebase/auth";
+
+
 	let email = '';
 	let password = '';
 	let errorMessage = '';
 
 	const signin = async () => {
+		let userCredential: UserCredential;
+		try {
+			userCredential = await signInWithEmailAndPassword(auth, email, password);
+			console.log('userCredential', userCredential);
+		} catch (err) {
+			if (isFirebaseError(err)) {
+				errorMessage = `[${err.code}] ${err.message}`;
+				return;
+			}
+			throw err;
+		}
+		const idToken = await userCredential.user.getIdToken();
 		await fetch('/session', {
 			method: 'POST',
-			body: JSON.stringify({"id_token": email}),
+			body: JSON.stringify({"id_token": idToken}),
 			headers: {'Content-Type': 'application/json'}
 		});
 		window.location.href = $page.url.origin + '/';
