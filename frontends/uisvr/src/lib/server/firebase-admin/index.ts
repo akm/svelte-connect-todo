604b4af1907b7b7164862d25fcfccefcd976a2ed
akm <akm2000@gmail.com>
2024-04-21 14:57:10 +0900

    :sparkles: Skip initializeApp if app instance already exists

--- frontends/uisvr/src/lib/server/firebase-admin/index.ts
+++ frontends/uisvr/src/lib/server/firebase-admin/index.ts
@@ -1,5 +1,21 @@
-import { initializeApp } from 'firebase-admin/app';
+import { initializeApp, getApp, getApps } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 
-const adminApp = initializeApp();
+// FirebaseAppError: The default Firebase app already exists.
+// This means you called initializeApp() more than once without
+// providing an app name as the second argument.
+// というエラーが npm run dev での実行時に発生していたので単純に
+// その場合は initializeApp() を呼ぶのではなく、apps.length で判定し、
+// 存在していた場合には app() を呼ぶように修正
+// See https://vitejs.dev/guide/env-and-mode
+const adminApp = (() => {
+	if (import.meta.env.DEV) {
+		return getApps().length === 0 ? initializeApp() : getApp();
+	} else {
+		return initializeApp();
+	}
+})();
+
 export const auth = getAuth(adminApp);
+
+export type { DecodedIdToken } from 'firebase-admin/auth';
