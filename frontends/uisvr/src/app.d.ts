eb928165120c146d480cbf118fe3c90b753339ab
akm <akm2000@gmail.com>
2024-04-21 14:53:44 +0900

    :sparkles: Define App.Locals.user

--- frontends/uisvr/src/app.d.ts
+++ frontends/uisvr/src/app.d.ts
@@ -1,9 +1,15 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import type { User } from '$lib/models/user';
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user: User | undefined;
+			// sessionID: string | undefined;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
