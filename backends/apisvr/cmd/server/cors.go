9eb47fe69ee6e41ddc90a036ffb30daf0959ab20
akm <akm2000@gmail.com>
2024-04-21 10:04:02 +0900

    :recycle: Extract constants for CORS option

--- backends/apisvr/cmd/server/cors.go
+++ backends/apisvr/cmd/server/cors.go
@@ -11,12 +11,19 @@ import (
 // https://github.com/connectrpc/cors-go
 
 // withCORS adds CORS support to a Connect HTTP handler.
+
+var (
+	allowedMethods = connectcors.AllowedMethods()
+	allowedHeaders = connectcors.AllowedHeaders()
+	exposedHeaders = connectcors.ExposedHeaders()
+)
+
 func withCORS(connectHandler http.Handler) http.Handler {
 	c := cors.New(cors.Options{
 		AllowedOrigins: []string{"http://localhost:5173", "http://localhost:4173"}, // replace with your domain
-		AllowedMethods: connectcors.AllowedMethods(),
-		AllowedHeaders: connectcors.AllowedHeaders(),
-		ExposedHeaders: connectcors.ExposedHeaders(),
+		AllowedMethods: allowedMethods,
+		AllowedHeaders: allowedHeaders,
+		ExposedHeaders: exposedHeaders,
 		MaxAge:         7200, // 2 hours in seconds
 	})
 	return c.Handler(connectHandler)
