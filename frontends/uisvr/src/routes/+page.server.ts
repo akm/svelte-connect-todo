54bd3ce99f818f942249834b3e40cd3413553cf0
akm <akm2000@gmail.com>
2024-04-23 08:04:46 +0900

    :sparkles: Redirect to sign in page unless sign in

--- frontends/uisvr/src/routes/+page.server.ts
+++ frontends/uisvr/src/routes/+page.server.ts
@@ -1,11 +1,16 @@
 import type { Task } from '$lib/models/task';
 import type { ServerLoadEvent } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 import { TaskService } from '../gen/task/v1/task_connect';
 import { createPromiseClient } from '@connectrpc/connect';
 import { createConnectTransport } from '@connectrpc/connect-web';
 import { Status } from '../gen/task/v1/task_pb';
 
 export async function load(event: ServerLoadEvent): Promise<{ tasks: Task[] }> {
+	if (!event.locals.user) {
+		throw redirect(302, '/signin');
+	}
+
 	console.log('load: event.constructor', event.constructor);
 
 	const transport = createConnectTransport({
