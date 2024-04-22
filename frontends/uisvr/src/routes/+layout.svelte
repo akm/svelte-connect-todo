e9aa2bab55f22efe7d26758fa8e217b7ac28ee75
akm <akm2000@gmail.com>
2024-04-23 07:59:27 +0900

    :sparkles: Add Sign out button on header

--- frontends/uisvr/src/routes/+layout.svelte
+++ frontends/uisvr/src/routes/+layout.svelte
@@ -2,6 +2,11 @@
   import "tailwindcss/tailwind.css";
 	import { page } from '$app/stores';
   const user = $page.data.user;
+
+  const signOut = async () => {
+    await fetch('/session', { method: 'DELETE' });
+    window.location.href = $page.url.origin + '/signin';
+  };
 </script>
 
 <header class="bg-gray-800 text-white p-4">
@@ -10,6 +15,7 @@
       <div class="text-sm">
         {#if user}
           <span class="font-medium">{user.name}</span>
+          <button class="btn ml-4" on:click={signOut}>Sign out</button>
         {:else}
           <a href="/signin" class="hover:underline">Sign in</a>
         {/if}
