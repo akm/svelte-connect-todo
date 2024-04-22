b6c0b74856d277b11973af8fe06ab455ea3fd8e9
akm <akm2000@gmail.com>
2024-04-23 07:29:47 +0900

    :sparkles: Add header at the top of page

--- frontends/uisvr/src/routes/+layout.svelte
+++ frontends/uisvr/src/routes/+layout.svelte
@@ -3,5 +3,17 @@
   import "tailwindcss/tailwind.css";
 </script>
 
-<slot />
+<header class="bg-gray-800 text-white p-4">
+  <div class="container mx-auto flex justify-between items-center">
+      <h1 class="text-lg font-bold">TODOs</h1>
+      <div class="text-sm">
+          User: <span class="font-medium">User name</span>
+      </div>
+  </div>
+</header>
 
+<main class="container mx-auto mt-8">
+
+  <slot />
+
+</main>
