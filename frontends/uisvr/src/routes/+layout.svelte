3af568e274363b370ae2e2713bd922c5ad8cbb46
akm <akm2000@gmail.com>
2024-04-23 07:51:02 +0900

    :sparkles: Show user name on header

--- frontends/uisvr/src/routes/+layout.svelte
+++ frontends/uisvr/src/routes/+layout.svelte
@@ -1,13 +1,18 @@
-
 <script>
   import "tailwindcss/tailwind.css";
+	import { page } from '$app/stores';
+  const user = $page.data.user;
 </script>
 
 <header class="bg-gray-800 text-white p-4">
   <div class="container mx-auto flex justify-between items-center">
       <h1 class="text-lg font-bold">TODOs</h1>
       <div class="text-sm">
-          User: <span class="font-medium">User name</span>
+        {#if user}
+          <span class="font-medium">{user.name}</span>
+        {:else}
+          <a href="/signin" class="hover:underline">Sign in</a>
+        {/if}
       </div>
   </div>
 </header>
