1bfaf1a3864e09a615f4f94312f59d78fc8c826b
akm <akm2000@gmail.com>
2024-04-23 21:07:02 +0900

    :sparkles: Add server settings and follow SIGTERM

--- backends/apisvr/cmd/server/main.go
+++ backends/apisvr/cmd/server/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"connectrpc.com/authn"
 	"golang.org/x/net/http2"
@@ -28,15 +34,37 @@ func main() {
 	if serverHostAndPort == "" {
 		port := os.Getenv("PORT")
 		if port == "" {
-			serverHostAndPort = "0.0.0.0:8080"
-		} else {
-			serverHostAndPort = "0.0.0.0:" + port
+			port = "8080"
 		}
+		serverHostAndPort = ":" + port
 	}
 
-	http.ListenAndServe(
-		serverHostAndPort,
-		// Use h2c so we can serve HTTP/2 without TLS.
-		withCORS(h2c.NewHandler(mux, &http2.Server{})),
-	)
+	// https://connectrpc.com/docs/go/deployment/
+	// https://github.com/connectrpc/examples-go/blob/main/cmd/demoserver/main.go
+	srv := &http.Server{
+		Addr: serverHostAndPort,
+		Handler: h2c.NewHandler(
+			withCORS(h2c.NewHandler(mux, &http2.Server{})),
+			&http2.Server{},
+		),
+		ReadHeaderTimeout: time.Second,
+		ReadTimeout:       5 * time.Minute,
+		WriteTimeout:      5 * time.Minute,
+		MaxHeaderBytes:    8 * 1024, // 8KiB
+	}
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("HTTP listen and serve: %v", err)
+		}
+	}()
+
+	<-signals
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("HTTP shutdown: %v", err) //nolint:gocritic
+	}
 }
