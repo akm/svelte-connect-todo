f9401b8ea51c480dcf723605795173fa1b3c8d57
akm <akm2000@gmail.com>
2024-04-27 19:12:45 +0900

    :sparkles: Add logging config to connect to apisvr in uisvr

--- frontends/uisvr/src/lib/apisvr/index.ts
+++ frontends/uisvr/src/lib/apisvr/index.ts
@@ -1,7 +1,19 @@
 import { browser } from '$app/environment';
 
+console.log('src/lib/apisvr/index.ts browser', browser);
+console.log(
+	'src/lib/apisvr/index.ts import.meta.env.VITE_APISVR_ORIGIN',
+	import.meta.env.VITE_APISVR_ORIGIN
+);
+console.log(
+	'src/lib/apisvr/index.ts import.meta.env.VITE_APISVR_ORIGIN_FROM_SERVER',
+	import.meta.env.VITE_APISVR_ORIGIN_FROM_SERVER
+);
+
 export const apisvrOrigin =
 	(browser
 		? import.meta.env.VITE_APISVR_ORIGIN
 		: import.meta.env.VITE_APISVR_ORIGIN_FROM_SERVER || import.meta.env.VITE_APISVR_ORIGIN) ??
 	'http://localhost:8080';
+
+console.log('src/lib/apisvr/index.ts apisvrOrigin', apisvrOrigin);
