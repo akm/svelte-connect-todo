f8b6901228d897f4e374b87458d8a0aea6d1e266
akm <akm2000@gmail.com>
2024-06-23 19:19:05 +0900

    :wrench: Add stages/production/secrets/firebaeconfig

--- stages/production/.config.mk
+++ stages/production/.config.mk
@@ -2,5 +2,6 @@ GOOGLE_CLOUD_PROJECT?=
 GCP_REGION?=asia-northeast1
 
 GCP_PROJECT_ID=$(GOOGLE_CLOUD_PROJECT)
+GCP_PROJECT_NUMBER=$(shell gcloud projects describe $(GOOGLE_CLOUD_PROJECT) --format 'value(projectNumber)')
 
 GCLOUD=gcloud --project $(GOOGLE_CLOUD_PROJECT)
