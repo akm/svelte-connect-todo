9ba72ace1cbdd135f6d41b6dc1b0e3884c6dd538
akm <akm2000@gmail.com>
2024-06-24 07:58:39 +0900

    :wrench: Fix test-connections in stages/production/apisvr/Makefile

--- backends/apisvr/test-connections.sh
+++ backends/apisvr/test-connections.sh
@@ -1,11 +1,9 @@
 set -x
 
-curl -v -L --header "Content-Type: application/json" --data '{}' https://${GCP_SERVICE_DOMAIN}/task.v1.TaskService/List
+curl -v -L --header "Content-Type: application/json" --data '{}' https://${SERVER_DOMAIN}/task.v1.TaskService/List
 
-buf curl --schema . --protocol connect https://${GCP_SERVICE_DOMAIN}/task.v1.TaskService/List
-buf curl --schema . --protocol grpcweb https://${GCP_SERVICE_DOMAIN}/task.v1.TaskService/List
-buf curl --schema . --protocol grpc https://${GCP_SERVICE_DOMAIN}/task.v1.TaskService/List
+buf curl --schema . --protocol connect https://${SERVER_DOMAIN}/task.v1.TaskService/List
+buf curl --schema . --protocol grpcweb https://${SERVER_DOMAIN}/task.v1.TaskService/List
+buf curl --schema . --protocol grpc https://${SERVER_DOMAIN}/task.v1.TaskService/List
 
-mkdir -p tmp
-buf build -o tmp/protoset.bin
-grpcurl -protoset tmp/protoset.bin ${GCP_SERVICE_DOMAIN}:443 task.v1.TaskService/List
+grpcurl -protoset proto/protoset.bin ${SERVER_DOMAIN}:443 task.v1.TaskService/List
