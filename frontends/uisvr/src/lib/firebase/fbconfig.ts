92cb40c62250c6645348265102f342caa6a0ac67
akm <akm2000@gmail.com>
2024-06-23 19:27:33 +0900

    :recycle: Rename firebaseconfig.ts to fbconfig.ts

---
+++ frontends/uisvr/src/lib/firebase/fbconfig.ts
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+	apiKey: 'dummy-firebase-api-key1',
+	authDomain: 'svelte-connect-todo.firebaseapp.com',
+	projectId: 'svelte-connect-todo-gcp-project1',
+	storageBucket: 'svelte-connect-todo.appspot.com',
+	messagingSenderId: '012345678901',
+	appId: '1:012345678901:web:0123456789abcdefghijkl',
+	measurementId: 'G-ZZZZZZZZZZ'
+};
