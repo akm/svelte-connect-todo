85df43da3faa1f5db3ffcc69146b260f3801a35c
akm <akm2000@gmail.com>
2024-06-29 11:41:12 +0900

    :sparkles: Add logging if use firebase emulator

--- frontends/uisvr/src/lib/firebase/auth/index.ts
+++ frontends/uisvr/src/lib/firebase/auth/index.ts
@@ -6,7 +6,9 @@ export const auth = getAuth(app);
 
 // https://firebase.google.com/docs/emulator-suite/connect_auth?hl=ja#web-modular-api
 if (env.SK_PUBLIC_FIREBASE_AUTH_EMULATOR_HOST) {
-	connectAuthEmulator(auth, 'http://' + env.SK_PUBLIC_FIREBASE_AUTH_EMULATOR_HOST);
+	const emulatorOrigin = 'http://' + env.SK_PUBLIC_FIREBASE_AUTH_EMULATOR_HOST;
+	console.log('Using Firebase Auth Emulator: ' + emulatorOrigin);
+	connectAuthEmulator(auth, emulatorOrigin);
 }
 
 export type { UserCredential } from 'firebase/auth';
