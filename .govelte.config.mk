6a78f4f5f7d97ce8dc7265169b967924b355a3c2
akm <akm2000@gmail.com>
2024-07-06 11:55:09 +0900

    :wrench: Follow renaming from goveltemk to shapeappmk

--- .govelte.config.mk
+++ .govelte.config.mk
@@ -3,7 +3,7 @@ APP_MYSQL_DATABASE_NAME=$(APP_BASE_NAME)-db1
 GOOGLE_CLOUD_PROJECT_LOCAL=$(APP_BASE_NAME)-gcp-project1
 APP_FIREBASE_API_KEY?=firebase-api-key-dummy1
 
-PATH_TO_GOVELTEMK=$(PATH_TO_PROJECT)/vendor/goveltemk
+PATH_TO_GOVELTEMK=$(PATH_TO_PROJECT)/vendor/shapeappmk
 include $(PATH_TO_GOVELTEMK)/makefiles/default/app_stage.mk
 include $(PATH_TO_GOVELTEMK)/makefiles/default/ports.mk
 include $(PATH_TO_GOVELTEMK)/makefiles/default/directories.mk
