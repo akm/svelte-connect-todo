265f9c1fcf7255c934c51d1cf5a4a7368d0d4127
akm <akm2000@gmail.com>
2024-07-06 17:12:27 +0900

    :wrench: :recycle: Extract .config.mk from .shapeapp.config.mk

---
+++ .config.mk
@@ -0,0 +1,4 @@
+APP_BASE_NAME=svelte-connect-todo
+APP_MYSQL_DATABASE_NAME=$(APP_BASE_NAME)-db1
+GOOGLE_CLOUD_PROJECT_LOCAL=$(APP_BASE_NAME)-gcp-project1
+APP_FIREBASE_API_KEY?=firebase-api-key-dummy1
--- .shapeapp.config.mk
+++ .shapeapp.config.mk
@@ -1,7 +1,4 @@
-APP_BASE_NAME=svelte-connect-todo
-APP_MYSQL_DATABASE_NAME=$(APP_BASE_NAME)-db1
-GOOGLE_CLOUD_PROJECT_LOCAL=$(APP_BASE_NAME)-gcp-project1
-APP_FIREBASE_API_KEY?=firebase-api-key-dummy1
+include $(PATH_TO_ROOT)/.config.mk
 
 PATH_TO_SHAPEAPPMK=$(PATH_TO_ROOT)/vendor/shapeappmk
 include $(PATH_TO_SHAPEAPPMK)/makefiles/default/app_stage.mk
