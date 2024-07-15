c829e6b3c47d03b194189b929c7945d63694e6fa
akm <akm2000@gmail.com>
2024-07-15 16:33:47 +0900

    :wrench: ディレクトリの構造変更に追随
    テストなどについては一度削除

--- .shapeapp.mk
+++ .shapeapp.mk
@@ -1,6 +1,6 @@
 include $(PATH_TO_ROOT)/.config.mk
 
 PATH_TO_SHAPEAPPMK=$(PATH_TO_ROOT)/vendor/shapeappmk
-include $(PATH_TO_SHAPEAPPMK)/makefiles/default/app_stage.mk
-include $(PATH_TO_SHAPEAPPMK)/makefiles/default/ports.mk
-include $(PATH_TO_SHAPEAPPMK)/makefiles/default/directories.mk
+include $(PATH_TO_SHAPEAPPMK)/default/app_stage.mk
+include $(PATH_TO_SHAPEAPPMK)/default/ports.mk
+include $(PATH_TO_SHAPEAPPMK)/default/directories.mk
