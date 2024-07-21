86d03a67027308490307f16da9010f6e975714fd
akm <akm2000@gmail.com>
2024-07-21 23:16:19 +0900

    :sparkles: backends/biz/queries/tasks.sql を追加

---
+++ backends/biz/queries/tasks.sql
@@ -0,0 +1,26 @@
+-- name: CountTasks :one
+SELECT count(*) FROM tasks;
+
+-- name: ListTasks :many
+SELECT * FROM tasks
+ORDER BY id ASC;
+
+-- name: GetTask :one
+SELECT * FROM tasks 
+WHERE id = ? LIMIT 1;
+
+-- name: CreateTask :execresult
+INSERT INTO tasks (
+  name, status
+) VALUES (
+  ?, ?
+);
+
+-- name: UpdateTask :exec
+UPDATE tasks
+SET name = ?, status = ?
+WHERE id = ?;
+
+-- name: DeleteTask :exec
+DELETE FROM tasks
+WHERE id = ?;
