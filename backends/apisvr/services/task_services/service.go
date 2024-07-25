c0d76204ff24d8964af05613859fa8fe68d24311
akm <akm2000@gmail.com>
2024-07-26 07:51:50 +0900

    👔 TaskService を実装し直しました

--- backends/apisvr/services/task_services/service.go
+++ backends/apisvr/services/task_services/service.go
@@ -2,6 +2,7 @@ package taskservices
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	v1 "apisvr/gen/task/v1"
@@ -14,35 +15,56 @@ type TaskService struct{}
 
 var _ taskv1connect.TaskServiceHandler = (*TaskService)(nil)
 
-type Task = v1.Task
-
-var taskStore = []*Task{
-	{Id: 1, Name: "task1", Status: v1.Status_STATUS_DONE},
-	{Id: 2, Name: "task2", Status: v1.Status_STATUS_TODO},
+type Task struct {
+	Id     uint64
+	Name   string
+	Status v1.TaskStatus
 }
 
-func (s *TaskService) List(context.Context, *connect.Request[v1.TaskListRequest]) (*connect.Response[v1.TaskListResponse], error) {
+var taskStore = []*Task{
+	{Id: 1, Name: "task1", Status: v1.TaskStatus_DONE},
+	{Id: 2, Name: "task2", Status: v1.TaskStatus_TODO},
+}
+
+func (s *TaskService) List(context.Context, *connect.Request[v1.TaskServiceListRequest]) (*connect.Response[v1.TaskServiceListResponse], error) {
 	log.Printf("TaskService.List")
-	resp := &connect.Response[v1.TaskListResponse]{
-		Msg: &v1.TaskListResponse{
-			Items: taskStore,
-			Total: uint64(len(taskStore)),
+
+	results := make([]*v1.TaskResponse, len(taskStore))
+	for i, task := range taskStore {
+		results[i] = &v1.TaskResponse{
+			Id:     task.Id,
+			Name:   task.Name,
+			Status: task.Status,
+		}
+	}
+
+	resp := &connect.Response[v1.TaskServiceListResponse]{
+		Msg: &v1.TaskServiceListResponse{
+			Items: results,
+			Total: uint64(len(results)),
 		},
 	}
 	return resp, nil
 }
 
-func (s *TaskService) Show(ctx context.Context, req *connect.Request[v1.TaskId]) (*connect.Response[v1.Task], error) {
+func (s *TaskService) Show(ctx context.Context, req *connect.Request[v1.ShowRequest]) (*connect.Response[v1.TaskResponse], error) {
 	log.Printf("TaskService.Show")
+
 	for _, task := range taskStore {
 		if task.Id == req.Msg.Id {
-			return &connect.Response[v1.Task]{Msg: task}, nil
+			result := &v1.TaskResponse{
+				Id:     task.Id,
+				Name:   task.Name,
+				Status: task.Status,
+			}
+			return &connect.Response[v1.TaskResponse]{Msg: result}, nil
 		}
 	}
-	return nil, nil
+
+	return nil, fmt.Errorf("task not found")
 }
 
-func (s *TaskService) Create(ctx context.Context, req *connect.Request[v1.TaskCreateRequest]) (*connect.Response[v1.Task], error) {
+func (s *TaskService) Create(ctx context.Context, req *connect.Request[v1.TaskServiceCreateRequest]) (*connect.Response[v1.TaskResponse], error) {
 	log.Printf("TaskService.Create")
 	var id uint64
 	if len(taskStore) == 0 {
@@ -56,27 +78,51 @@ func (s *TaskService) Create(ctx context.Context, req *connect.Request[v1.TaskCr
 		Status: req.Msg.Status,
 	}
 	taskStore = append(taskStore, task)
-	return &connect.Response[v1.Task]{Msg: task}, nil
+
+	result := &v1.TaskResponse{
+		Id:     task.Id,
+		Name:   task.Name,
+		Status: task.Status,
+	}
+	return &connect.Response[v1.TaskResponse]{Msg: result}, nil
 }
 
-func (s *TaskService) Update(ctx context.Context, req *connect.Request[v1.Task]) (*connect.Response[v1.Task], error) {
+func (s *TaskService) Update(ctx context.Context, req *connect.Request[v1.TaskServiceUpdateRequest]) (*connect.Response[v1.TaskResponse], error) {
 	log.Printf("TaskService.Update")
 	for i, task := range taskStore {
 		if task.Id == req.Msg.Id {
-			taskStore[i] = req.Msg
-			return &connect.Response[v1.Task]{Msg: req.Msg}, nil
+			task := &Task{
+				Id:     req.Msg.Id,
+				Name:   req.Msg.Name,
+				Status: req.Msg.Status,
+			}
+			taskStore[i] = task
+
+			result := &v1.TaskResponse{
+				Id:     task.Id,
+				Name:   task.Name,
+				Status: task.Status,
+			}
+			return &connect.Response[v1.TaskResponse]{Msg: result}, nil
 		}
 	}
-	return nil, nil
+	return nil, fmt.Errorf("task not found")
 }
 
-func (s *TaskService) Delete(ctx context.Context, req *connect.Request[v1.TaskId]) (*connect.Response[v1.TaskId], error) {
+func (s *TaskService) Delete(ctx context.Context, req *connect.Request[v1.DeleteRequest]) (*connect.Response[v1.TaskResponse], error) {
 	log.Printf("TaskService.Delete")
 	for i, task := range taskStore {
 		if task.Id == req.Msg.Id {
+			task := taskStore[i]
 			taskStore = append(taskStore[:i], taskStore[i+1:]...)
-			return &connect.Response[v1.TaskId]{Msg: req.Msg}, nil
+
+			result := &v1.TaskResponse{
+				Id:     task.Id,
+				Name:   task.Name,
+				Status: task.Status,
+			}
+			return &connect.Response[v1.TaskResponse]{Msg: result}, nil
 		}
 	}
-	return nil, nil
+	return nil, fmt.Errorf("task not found")
 }
