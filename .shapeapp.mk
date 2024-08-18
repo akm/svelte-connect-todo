1c3a36b7d8a52173763ff86be42f297b1e741646
akm <akm2000@gmail.com>
2024-08-18 14:10:14 +0900

    🔨 shell-dir-target-vars を使うように変更

--- .shapeapp.mk
+++ .shapeapp.mk
@@ -2,6 +2,7 @@ include $(PATH_TO_ROOT)/.config.mk
 
 PATH_TO_SHAPEAPPMK=$(PATH_TO_ROOT)/vendor/shapeappmk
 include $(PATH_TO_SHAPEAPPMK)/make/debug.mk
+include $(PATH_TO_SHAPEAPPMK)/make/shell.mk
 include $(PATH_TO_SHAPEAPPMK)/default/app_stage.mk
 include $(PATH_TO_SHAPEAPPMK)/default/ports.mk
 include $(PATH_TO_SHAPEAPPMK)/default/directories.mk
