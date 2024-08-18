b20b87bcb851ea4d6c0427f8091939b2113d505d
akm <akm2000@gmail.com>
2024-08-18 16:58:01 +0900

    🔨♻️ asdf/reshim.mk と text-template-cli/base.mk を.shapeapp.mk で include

--- .shapeapp.mk
+++ .shapeapp.mk
@@ -3,6 +3,8 @@ include $(PATH_TO_ROOT)/.config.mk
 PATH_TO_SHAPEAPPMK=$(PATH_TO_ROOT)/vendor/shapeappmk
 include $(PATH_TO_SHAPEAPPMK)/make/debug.mk
 include $(PATH_TO_SHAPEAPPMK)/make/shell.mk
+include $(PATH_TO_SHAPEAPPMK)/asdf/reshim.mk
+include $(PATH_TO_SHAPEAPPMK)/text-template-cli/base.mk
 include $(PATH_TO_SHAPEAPPMK)/default/app_stage.mk
 include $(PATH_TO_SHAPEAPPMK)/default/ports.mk
 include $(PATH_TO_SHAPEAPPMK)/default/directories.mk
