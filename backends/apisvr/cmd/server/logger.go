c7a1d70baae34899b55124d85e8f7bf10d94096d
akm <akm2000@gmail.com>
2024-08-26 21:11:46 +0900

    ✨ 環境変数 LOG_LEVEL が設定されていない場合のデフォルトを INFO に設定

--- backends/apisvr/cmd/server/logger.go
+++ backends/apisvr/cmd/server/logger.go
@@ -7,8 +7,12 @@ import (
 )
 
 func newLogger() (*slog.Logger, error) {
+	logLevelStr := os.Getenv("LOG_LEVEL")
+	if logLevelStr == "" {
+		logLevelStr = "INFO"
+	}
 	var level slog.Level
-	if err := level.UnmarshalText([]byte(os.Getenv("LOG_LEVEL"))); err != nil {
+	if err := level.UnmarshalText([]byte(logLevelStr)); err != nil {
 		return nil, err
 	}
 	opts := &slog.HandlerOptions{Level: level}
