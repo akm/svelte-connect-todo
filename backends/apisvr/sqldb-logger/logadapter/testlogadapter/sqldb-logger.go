0d0791a6385813a54c87fe97cf986812d88a4595
akm <akm2000@gmail.com>
2024-08-29 22:40:18 +0900

    ♻️ test用の sqldblogger.Logger を 他の実装の組み合わせで実現

--- backends/apisvr/sqldb-logger/logadapter/testlogadapter/sqldb-logger.go
+++ backends/apisvr/sqldb-logger/logadapter/testlogadapter/sqldb-logger.go
@@ -1,44 +1,19 @@
 package testlogadapter
 
-// Copied from https://github.com/simukti/sqldb-logger/pull/76
-
 import (
-	"context"
-	"log/slog"
 	"testing"
 
-	sqldblogger "github.com/simukti/sqldb-logger"
-)
+	"applib/log/slog"
+	"applib/testing/testio"
 
-type slogAdapter struct {
-	t *testing.T
-}
+	sqldblogger "github.com/simukti/sqldb-logger"
+
+	slogadapter "apisvr/sqldb-logger/logadapter/slogadapter"
+)
 
 // New creates a log adapter from sqldblogger.Logger to an slog.Logger one.
 func New(t *testing.T) sqldblogger.Logger {
-	return &slogAdapter{t: t}
-}
-
-// Log implement sqldblogger.Logger and converts its levels to corresponding
-// log/slog ones.
-func (a *slogAdapter) Log(ctx context.Context, sqldbLevel sqldblogger.Level, msg string, data map[string]interface{}) {
-	attrs := make([]slog.Attr, 0, len(data))
-	for k, v := range data {
-		attrs = append(attrs, slog.Any(k, v))
-	}
-
-	var level slog.Level
-	switch sqldbLevel {
-	case sqldblogger.LevelError:
-		level = slog.LevelError
-	case sqldblogger.LevelInfo:
-		level = slog.LevelInfo
-	case sqldblogger.LevelDebug:
-		level = slog.LevelDebug
-	default:
-		// trace will use slog debug
-		level = slog.LevelDebug
-	}
-
-	a.t.Logf("[%s] %s %v", level, msg, attrs)
+	return slogadapter.New(
+		slog.NewLogger(testio.New(t), slog.LevelDebug, slog.NewTextHandler),
+	)
 }
