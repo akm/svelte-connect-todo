2033980a099d65d7994c417f7be2ef342a9a3146
akm <akm2000@gmail.com>
2024-09-01 14:54:52 +0900

    🔧 golang install 周りの変更を使えるように shapeappmk を更新

--- .shapeapp.mk
+++ .shapeapp.mk
@@ -4,6 +4,7 @@ PATH_TO_SHAPEAPPMK=$(PATH_TO_ROOT)/vendor/shapeappmk
 include $(PATH_TO_SHAPEAPPMK)/make/debug.mk
 include $(PATH_TO_SHAPEAPPMK)/make/shell.mk
 include $(PATH_TO_SHAPEAPPMK)/asdf/reshim.mk
+include $(PATH_TO_SHAPEAPPMK)/golang/tool.mk
 include $(PATH_TO_SHAPEAPPMK)/text-template-cli/base.mk
 include $(PATH_TO_SHAPEAPPMK)/default/app_stage.mk
 include $(PATH_TO_SHAPEAPPMK)/default/ports.mk
