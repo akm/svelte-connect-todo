6d4b06fc52482816028e0d765f33b8534e274eba
akm <akm2000@gmail.com>
2024-09-14 23:38:45 +0900

    ✅ TaskList.stories.ts を追加

---
+++ frontends/uisvr/src/stories/TaskList.stories.ts
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/svelte';
+
+import 'tailwindcss/tailwind.css';
+import TaskList from '$lib/components/collections/TaskList.svelte';
+
+const meta = {
+	title: 'Example/TaskList',
+	component: TaskList,
+	// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+	tags: ['autodocs'],
+	parameters: {
+		// More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+		layout: 'fullscreen'
+	}
+} satisfies Meta<TaskList>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+	args: {
+		tasks: [
+			{
+				id: 1,
+				name: 'Task 1',
+				done: true
+			},
+			{
+				id: 2,
+				name: 'Task 2',
+				done: false
+			},
+			{
+				id: 3,
+				name: 'Long name task 3 qwertyuiopasdfghjklzxcvbnm',
+				done: false
+			}
+		]
+	}
+};
+
+export const Empty: Story = {
+	args: {
+		tasks: []
+	}
+};
