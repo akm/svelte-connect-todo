0861143ba78a338bf5f7c54b6d1e3f45aa0fede0
akm <akm2000@gmail.com>
2024-10-13 18:19:44 +0900

    ♻️ TaskService のメソッドの戻り値に変数を設定

--- backends/apisvr/services/task_services/service.go
+++ backends/apisvr/services/task_services/service.go
@@ -30,103 +30,56 @@ type Task struct {
 	Status v1.TaskStatus
 }
 
-func (s *TaskService) List(ctx context.Context, req *connect.Request[v1.TaskServiceListRequest]) (*connect.Response[v1.TaskServiceListResponse], error) {
-	s.StartAction(ctx, "List")
+func (s *TaskService) List(ctx context.Context, req *connect.Request[v1.TaskServiceListRequest]) (resp *connect.Response[v1.TaskServiceListResponse], rerr error) {
+	rerr = func() error {
+		s.StartAction(ctx, "List")
 
-	queries := models.New(s.Pool)
-	dbTasks, err := queries.ListTasks(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	results := make([]*v1.TaskResponse, len(dbTasks))
-	for i, task := range dbTasks {
-		var st v1.TaskStatus
-		switch task.Status {
-		case models.TasksStatusTodo:
-			st = v1.TaskStatus_TODO
-		case models.TasksStatusDone:
-			st = v1.TaskStatus_DONE
-		default:
-			st = v1.TaskStatus_UNKNOWN_UNSPECIFIED
+		queries := models.New(s.Pool)
+		dbTasks, err := queries.ListTasks(ctx)
+		if err != nil {
+			return err
 		}
 
-		results[i] = &v1.TaskResponse{
-			Id:     task.ID,
-			Name:   task.Name,
-			Status: st,
+		results := make([]*v1.TaskResponse, len(dbTasks))
+		for i, task := range dbTasks {
+			var st v1.TaskStatus
+			switch task.Status {
+			case models.TasksStatusTodo:
+				st = v1.TaskStatus_TODO
+			case models.TasksStatusDone:
+				st = v1.TaskStatus_DONE
+			default:
+				st = v1.TaskStatus_UNKNOWN_UNSPECIFIED
+			}
+
+			results[i] = &v1.TaskResponse{
+				Id:     task.ID,
+				Name:   task.Name,
+				Status: st,
+			}
 		}
-	}
 
-	resp := &connect.Response[v1.TaskServiceListResponse]{
-		Msg: &v1.TaskServiceListResponse{
-			Items: results,
-			Total: uint64(len(results)),
-		},
-	}
-	return resp, nil
+		resp = &connect.Response[v1.TaskServiceListResponse]{
+			Msg: &v1.TaskServiceListResponse{
+				Items: results,
+				Total: uint64(len(results)),
+			},
+		}
+		return nil
+	}()
+	return
 }
 
-func (s *TaskService) Show(ctx context.Context, req *connect.Request[v1.ShowRequest]) (*connect.Response[v1.TaskResponse], error) {
-	s.StartAction(ctx, "Show")
+func (s *TaskService) Show(ctx context.Context, req *connect.Request[v1.ShowRequest]) (resp *connect.Response[v1.TaskResponse], rerr error) {
+	rerr = func() error {
+		s.StartAction(ctx, "Show")
 
-	queries := models.New(s.Pool)
-	task, err := queries.GetTask(ctx, req.Msg.Id)
-	if err != nil {
-		return nil, s.ToConnectError(err)
-	}
+		queries := models.New(s.Pool)
+		task, err := queries.GetTask(ctx, req.Msg.Id)
+		if err != nil {
+			return s.ToConnectError(err)
+		}
 
-	var st v1.TaskStatus
-	switch task.Status {
-	case models.TasksStatusTodo:
-		st = v1.TaskStatus_TODO
-	case models.TasksStatusDone:
-		st = v1.TaskStatus_DONE
-	default:
-		st = v1.TaskStatus_UNKNOWN_UNSPECIFIED
-	}
-
-	result := &v1.TaskResponse{
-		Id:     task.ID,
-		Name:   task.Name,
-		Status: st,
-	}
-	return &connect.Response[v1.TaskResponse]{Msg: result}, nil
-}
-
-func (s *TaskService) Create(ctx context.Context, req *connect.Request[v1.TaskServiceCreateRequest]) (*connect.Response[v1.TaskResponse], error) {
-	s.StartAction(ctx, "Create")
-
-	if err := s.ValidateMsg(ctx, req.Msg); err != nil {
-		return nil, err
-	}
-
-	queries := models.New(s.Pool)
-
-	var st models.TasksStatus
-	switch req.Msg.Status {
-	case v1.TaskStatus_TODO:
-		st = models.TasksStatusTodo
-	case v1.TaskStatus_DONE:
-		st = models.TasksStatusDone
-	default:
-		return nil, fmt.Errorf("unknown status: %v", req.Msg.Status)
-	}
-
-	task := models.CreateTaskParams{
-		Name:   req.Msg.Name,
-		Status: st,
-	}
-	res, err := queries.CreateTask(ctx, task)
-	if err != nil {
-		return nil, err
-	}
-	id, err := res.LastInsertId()
-	if err != nil {
-		return nil, err
-	}
-
-	{
 		var st v1.TaskStatus
 		switch task.Status {
 		case models.TasksStatusTodo:
@@ -138,26 +91,25 @@ func (s *TaskService) Create(ctx context.Context, req *connect.Request[v1.TaskSe
 		}
 
 		result := &v1.TaskResponse{
-			Id:     uint64(id),
+			Id:     task.ID,
 			Name:   task.Name,
 			Status: st,
 		}
-		return &connect.Response[v1.TaskResponse]{Msg: result}, nil
-	}
+		resp = &connect.Response[v1.TaskResponse]{Msg: result}
+		return nil
+	}()
+	return
 }
 
-func (s *TaskService) Update(ctx context.Context, req *connect.Request[v1.TaskServiceUpdateRequest]) (res *connect.Response[v1.TaskResponse], rerr error) {
-	s.StartAction(ctx, "Update")
+func (s *TaskService) Create(ctx context.Context, req *connect.Request[v1.TaskServiceCreateRequest]) (resp *connect.Response[v1.TaskResponse], rerr error) {
+	rerr = func() error {
+		s.StartAction(ctx, "Create")
 
-	if err := s.ValidateMsg(ctx, req.Msg); err != nil {
-		return nil, err
-	}
-
-	rerr = s.Transaction(ctx, func(tx *sql.Tx) error {
-		qtx := models.New(s.Pool).WithTx(tx)
-		if _, err := qtx.GetTaskForUpdate(ctx, req.Msg.Id); err != nil {
-			return s.ToConnectError(err)
+		if err := s.ValidateMsg(ctx, req.Msg); err != nil {
+			return err
 		}
+
+		queries := models.New(s.Pool)
 
 		var st models.TasksStatus
 		switch req.Msg.Status {
@@ -169,13 +121,17 @@ func (s *TaskService) Update(ctx context.Context, req *connect.Request[v1.TaskSe
 			return fmt.Errorf("unknown status: %v", req.Msg.Status)
 		}
 
-		task := models.UpdateTaskParams{
-			ID:     req.Msg.Id,
+		task := models.CreateTaskParams{
 			Name:   req.Msg.Name,
 			Status: st,
 		}
-		if err := qtx.UpdateTask(ctx, task); err != nil {
-			return s.ToConnectError(err)
+		res, err := queries.CreateTask(ctx, task)
+		if err != nil {
+			return err
+		}
+		id, err := res.LastInsertId()
+		if err != nil {
+			return err
 		}
 
 		{
@@ -190,45 +146,106 @@ func (s *TaskService) Update(ctx context.Context, req *connect.Request[v1.TaskSe
 			}
 
 			result := &v1.TaskResponse{
-				Id:     req.Msg.Id,
+				Id:     uint64(id),
 				Name:   task.Name,
 				Status: st,
 			}
-			res = &connect.Response[v1.TaskResponse]{Msg: result}
+			resp = &connect.Response[v1.TaskResponse]{Msg: result}
+			return nil
 		}
-		return nil
-	})
+	}()
 	return
 }
 
-func (s *TaskService) Delete(ctx context.Context, req *connect.Request[v1.DeleteRequest]) (*connect.Response[v1.TaskResponse], error) {
-	s.StartAction(ctx, "Delete")
+func (s *TaskService) Update(ctx context.Context, req *connect.Request[v1.TaskServiceUpdateRequest]) (resp *connect.Response[v1.TaskResponse], rerr error) {
+	rerr = func() error {
+		s.StartAction(ctx, "Update")
 
-	queries := models.New(s.Pool)
-	task, err := queries.GetTask(ctx, req.Msg.Id)
-	if err != nil {
-		return nil, s.ToConnectError(err)
-	}
+		if err := s.ValidateMsg(ctx, req.Msg); err != nil {
+			return err
+		}
 
-	var st v1.TaskStatus
-	switch task.Status {
-	case models.TasksStatusTodo:
-		st = v1.TaskStatus_TODO
-	case models.TasksStatusDone:
-		st = v1.TaskStatus_DONE
-	default:
-		st = v1.TaskStatus_UNKNOWN_UNSPECIFIED
-	}
+		return s.Transaction(ctx, func(tx *sql.Tx) error {
+			qtx := models.New(s.Pool).WithTx(tx)
+			if _, err := qtx.GetTaskForUpdate(ctx, req.Msg.Id); err != nil {
+				return s.ToConnectError(err)
+			}
 
-	result := &v1.TaskResponse{
-		Id:     task.ID,
-		Name:   task.Name,
-		Status: st,
-	}
+			var st models.TasksStatus
+			switch req.Msg.Status {
+			case v1.TaskStatus_TODO:
+				st = models.TasksStatusTodo
+			case v1.TaskStatus_DONE:
+				st = models.TasksStatusDone
+			default:
+				return fmt.Errorf("unknown status: %v", req.Msg.Status)
+			}
 
-	if err := queries.DeleteTask(ctx, req.Msg.Id); err != nil {
-		return nil, err
-	}
+			task := models.UpdateTaskParams{
+				ID:     req.Msg.Id,
+				Name:   req.Msg.Name,
+				Status: st,
+			}
+			if err := qtx.UpdateTask(ctx, task); err != nil {
+				return s.ToConnectError(err)
+			}
 
-	return &connect.Response[v1.TaskResponse]{Msg: result}, nil
+			{
+				var st v1.TaskStatus
+				switch task.Status {
+				case models.TasksStatusTodo:
+					st = v1.TaskStatus_TODO
+				case models.TasksStatusDone:
+					st = v1.TaskStatus_DONE
+				default:
+					st = v1.TaskStatus_UNKNOWN_UNSPECIFIED
+				}
+
+				result := &v1.TaskResponse{
+					Id:     req.Msg.Id,
+					Name:   task.Name,
+					Status: st,
+				}
+				resp = &connect.Response[v1.TaskResponse]{Msg: result}
+			}
+			return nil
+		})
+	}()
+	return
+}
+
+func (s *TaskService) Delete(ctx context.Context, req *connect.Request[v1.DeleteRequest]) (resp *connect.Response[v1.TaskResponse], rerr error) {
+	rerr = func() error {
+		s.StartAction(ctx, "Delete")
+
+		queries := models.New(s.Pool)
+		task, err := queries.GetTask(ctx, req.Msg.Id)
+		if err != nil {
+			return s.ToConnectError(err)
+		}
+
+		var st v1.TaskStatus
+		switch task.Status {
+		case models.TasksStatusTodo:
+			st = v1.TaskStatus_TODO
+		case models.TasksStatusDone:
+			st = v1.TaskStatus_DONE
+		default:
+			st = v1.TaskStatus_UNKNOWN_UNSPECIFIED
+		}
+
+		result := &v1.TaskResponse{
+			Id:     task.ID,
+			Name:   task.Name,
+			Status: st,
+		}
+
+		if err := queries.DeleteTask(ctx, req.Msg.Id); err != nil {
+			return err
+		}
+
+		resp = &connect.Response[v1.TaskResponse]{Msg: result}
+		return nil
+	}()
+	return
 }
